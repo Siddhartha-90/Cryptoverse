fc88967cf094ff1194435ac660da7cd2ccf7b05c
Siddhartha-90 <sidsrivas90@gmail.com>
2023-07-28 13:13:57 +0530

    All the error is been fixed

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,37 +1,37 @@
- import React, { useEffect } from 'react'
- import millify from 'millify'
- import {Link} from 'react-router-dom'
- import {Card, Row, Col, Input} from 'antd'
- import { useGetCryptosQuery } from '../Services/cryptoApi'
+import React, { useEffect } from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { Card, Row, Col, Input } from 'antd'
+import { useGetCryptosQuery } from '../Services/cryptoApi'
 import { useState } from 'react'
- import Loader from './Loader'
+import Loader from './Loader'
 
- 
- const Cryptocurrencies = ({simplified}) => {
-    const count= simplified ? 10:100
-    const {data:cryptoList, isFetching} =useGetCryptosQuery(count)
-    const [cryptos, setCryptos]= useState(cryptoList?.data?.coins)
-    // const [SearchTerm , setSearchTerm]=useState('')
 
-    // useEffect(()=>{
-    //      const filteredData=cryptoList?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(SearchTerm.toLowerCase))
-    //    setCryptos(filteredData)
-    // } , [cryptoList, SearchTerm])
-    if(isFetching) return <Loader/>
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+  const [searchTerm, setSearchTerm] = useState('')
 
-   return (
-     <>
+  useEffect(() => {
+    const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setCryptos(filteredData)
+  }, [cryptoList, searchTerm])
+  if (isFetching) return <Loader />
+
+  return (
+    <>
       <div className='search-crypto'>
-        {/* <Input placeholder='Search Crypto Currency' onChange={ (e)=> setSearchTerm(e.target.value)}/>  */}
+        <Input placeholder='Search Crypto Currency' onChange={(e) => setSearchTerm(e.target.value)} />
 
       </div>
-      <Row gutter={[32,32]} className='crypto-card-container'>
-         {cryptos?.map((currency)=>(
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={8} className='crypto-card' key={currency.uuid}>
             <Link to={`/crypto/${currency.uuid}`}>
               <Card title={`${currency.rank}. ${currency.name}`}
-              extra={<img className='crypto-image' src={currency.iconUrl}/>}
-              hoverable              
+                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                hoverable
               >
                 <p>Price:{millify(currency.price)}</p>
                 <p>Market Cap:{millify(currency.marketCap)}</p>
@@ -43,11 +43,11 @@ import { useState } from 'react'
 
 
 
-         ))}
+        ))}
 
       </Row>
-     </>
-   )
- }
- 
- export default Cryptocurrencies
+    </>
+  )
+}
+
+export default Cryptocurrencies
